2d87261bb1bf702f56090344aba3601e0b540930
Matt Daloisio <mattdam1@yahoo.co.uk>
2020-11-13 18:24:20 +0000

    no root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'cocktails#index'
+  # root to: 'cocktails#index'
   resources :cocktails, only: [:index, :new, :create, :show, :edit, :update] do
     resources :doses, only: [:new, :create, :show]
     resources :reviews, only: [:new, :create]
